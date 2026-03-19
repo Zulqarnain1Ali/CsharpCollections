0f14cde4ed6e046370b560127e7c848d3438636b
Zulqarnain Ali Natiq <zulqarnain15ali@gmail.com>
2026-03-19 19:19:13 +0500

    strongly typed generic collections. list

--- Collections in C/Program.cs
+++ Collections in C/Program.cs
@@ -51,32 +51,72 @@ namespace Collections_in_C
 
             // hashtables
 
-            Hashtable ht = new Hashtable();
-            ht.Add(1, "john");
-            ht.Add(2, 134);
-            ht.Add(2.1, "string");
-            ht.Add(2.2, "Max");
-            ht.Add(true, 23);
-            ht.Add(false, true);
+            //Hashtable ht = new Hashtable();
+            //ht.Add(1, "john");
+            //ht.Add(2, 134);
+            //ht.Add(2.1, "string");
+            //ht.Add(2.2, "Max");
+            //ht.Add(true, 23);
+            //ht.Add(false, true);
 
-            //foreach(var item in ht)
+            //ht.Remove(2.1);
+            ////foreach(var item in ht)
+            ////{
+            ////    var entry = (DictionaryEntry)item;
+            ////    Console.WriteLine(entry.Key +""+ entry.Value); 
+            ////}
+
+            //foreach (var item in ht)
             //{
             //    var entry = (DictionaryEntry)item;
-            //    Console.WriteLine(entry.Key +""+ entry.Value); 
+            //    Console.WriteLine($"Key: {entry.Key} | Value: {entry.Value}");
             //}
 
-            foreach (var item in ht)
-            {
-                var entry = (DictionaryEntry)item;
-                Console.WriteLine($"Key: {entry.Key} | Value: {entry.Value}");
-            }
+            //Console.WriteLine(ht.ContainsKey(1.1));
+            //Console.WriteLine(ht.ContainsValue(1));
+
 
             //interpolation
             //string a = "ali";
             //string b = "20";
             //Console.WriteLine($"my name is {a} and age is {b}");
 
-            
+
+
+
+            //_________________________________________________________________________________________________________________________
+
+
+            //generic collections           strongly typed
+            //list
+
+            List<string> food = new List<string>();
+
+            food.Add("Piza");
+            food.Add("burger");
+            food.Add("hotdog");
+            food.Add("fries");
+
+            //food.Remove("fries");
+
+            Console.WriteLine(food.Count);
+
+            //Console.WriteLine(food.Contains("Piza"));
+            //Console.WriteLine(food.IndexOf("burger"));
+            //Console.WriteLine(food.LastIndexOf("fries"));
+            //food.Sort();
+            //food.Reverse();
+            //food.Clear();
+
+            //Convert into array
+            //String[] foodArray = food.ToArray();
+
+            foreach (string item in food)
+            {
+                Console.WriteLine(item);
+            }
+            //Console.WriteLine(food[0]);
+
 
 
         }
