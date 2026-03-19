c5998471e4bab9b9dd34a646f2af9bb84ade024e
Zulqarnain Ali Natiq <zulqarnain15ali@gmail.com>
2026-03-19 17:11:42 +0500

    wrote a hastable

--- Collections in C/Program.cs
+++ Collections in C/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+
+
 
 namespace Collections_in_C
 {
@@ -10,6 +13,71 @@ namespace Collections_in_C
     {
         static void Main(string[] args)
         {
+
+            ////Array list
+            //ArrayList arrList = new ArrayList();
+            //arrList.Add(2);
+            //arrList.Add("ali");
+            //arrList.Add(null);
+            //arrList.Add(2.3);
+
+
+            //int[] values = new[] {2, 3, 4 };
+            //arrList.AddRange(values);
+            //string[] names = { "name1", "name2", "name3" };
+            //arrList.AddRange(names);
+
+            ////or
+
+            ////arrList.AddRange(new[] { 1, 2, 3 });
+
+            //arrList.Insert(1, "adding a string");
+
+            //arrList.InsertRange(2, names);
+
+            //arrList.RemoveAt(3);
+
+
+            //foreach (var item in arrList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+
+
+
+            // hashtables
+
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "john");
+            ht.Add(2, 134);
+            ht.Add(2.1, "string");
+            ht.Add(2.2, "Max");
+            ht.Add(true, 23);
+            ht.Add(false, true);
+
+            //foreach(var item in ht)
+            //{
+            //    var entry = (DictionaryEntry)item;
+            //    Console.WriteLine(entry.Key +""+ entry.Value); 
+            //}
+
+            foreach (var item in ht)
+            {
+                var entry = (DictionaryEntry)item;
+                Console.WriteLine($"Key: {entry.Key} | Value: {entry.Value}");
+            }
+
+            //interpolation
+            //string a = "ali";
+            //string b = "20";
+            //Console.WriteLine($"my name is {a} and age is {b}");
+
+            
+
 
         }
     }
---
+++ Collections in C/class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_in_C
+{
+    internal class @class
+    {
+
+    }
+}
